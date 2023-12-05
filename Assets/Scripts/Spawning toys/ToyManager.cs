6078c4a5d26fb947490af4aba21f5fa283af6afc
Jannire Trujillo <Janniretruji@gmail.com>
2023-12-05 18:42:50 -0500

    Add food, nivel manager (sin vida)

--- Assets/Scripts/Spawning toys/ToyManager.cs
+++ Assets/Scripts/Spawning toys/ToyManager.cs
@@ -7,6 +7,7 @@ public class ToyManager : MonoBehaviour
     // Start is called before the first frame update
     public List<string> listaObtenida = new List<string>();
     public List<string> listaPorObtener = new List<string>();
+    public int nivel = 0;
 
     public static ToyManager Instance { get; private set; }
     private void Awake()
@@ -16,7 +17,7 @@ public class ToyManager : MonoBehaviour
 
     void Start()
     {
-
+        nextNivel();
     }
 
     // Update is called once per frame
@@ -25,11 +26,38 @@ public class ToyManager : MonoBehaviour
 
     }
 
+
+
+    public void nextNivel()
+    {
+        nivel++;
+        listaObtenida.Clear();
+        switch (nivel)
+        {
+            case 1:
+                listaPorObtener = new List<string> { "ToyBall(Clone)", "ToyBall(Clone)", "ToySkate(Clone)", "ToyBear(Clone)" };
+                break;
+            case 2:
+                listaPorObtener = new List<string> { "Toy(Clone)", "ToySkate(Clone)", "ToyBear(Clone)", "ToySkate(Clone)", "ToyBear(Clone)", "ToyBall(Clone)" };
+                break;
+            default:
+                break;
+        }
+    }
+
     public void QuitardeLista(string toy)
     {
-        Debug.Log("Grabbed: " + toy);
-        //listaPorObtener.Remove(toy);
-        listaObtenida.Add(toy);
+        if (listaPorObtener.Contains(toy))
+        {
+            Debug.Log("Grabbed: " + toy);
+            listaPorObtener.Remove(toy);
+            listaObtenida.Add(toy);
+        }
+        if(listaPorObtener.Count == 0)
+        {
+            Debug.Log("Next level!");
+            nextNivel();
+        }
     }
 
 }
